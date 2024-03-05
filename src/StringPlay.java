a7bfc1130030e7ab911d72871b8f014054180f19
NickyMack1 <n.makubika5583@sudent.leedsbeckett.ac.uk>
2024-03-05 14:06:39 +0000

    StringPlay.java

---
+++ src/StringPlay.java
@@ -0,0 +1,19 @@
+public class StringPlay
+{
+    public static void main (String[] args)
+    {
+        String college = "Leeds Beckett University";
+        String town = "Anytown, UK"; // part (a)
+
+        int stringLength;
+        String change3;
+
+        stringLength = college.length(); // part (b)
+
+        System.out.println (college + " contains " + stringLength + " characters.");
+
+        change3 = college.concat(town); // part (e)
+
+        System.out.println ("The final string is " + change3);
+    }
+}
